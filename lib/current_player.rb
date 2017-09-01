4358a3e3875612365178ad869e163dfb526747a9
oomeru <obaroomeru@gmail.com>
2017-09-01 15:34:55 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,3 +9,15 @@ def turn_count(board)
   end
   return counter
 end
+
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    current_player = "X"
+  else 
+    current_player = "O"
+  end
+  return current_player
+end
+
+  
