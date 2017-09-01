1a0057b523096de750c9be30c5607cb6095b59c9
oomeru <obaroomeru@gmail.com>
2017-09-01 15:32:31 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+board = [" ", " ", " ", " ", " ", " "," ", " ", " " ]
+
+def turn_count(board)
+  counter = 0
+  board.each do | space |
+    if space == "X" || space == "0"
+      counter +=1
+    end
+  end
+  return counter
+end
+    
+    
+  
